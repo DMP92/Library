6cc95f8f5b1baeb6422027754cf5a09aa6ea7b31
Google Project <devin.pfromm.20@gmail.com>
2021-05-06 13:00:42 -0400

    Added modal menu animation
    Also added content to the modal. Although, this will probably be greatly altered. These changes will however allow me to begin putting together the JS for book objects.

--- main.js
+++ main.js
@@ -11,5 +11,40 @@ function addBookToLibrary() {
 
 }
 
+var clicked = false;
+let modalButton = document.querySelector('.linedNavDiv');
+let modalPop = document.querySelector('.modal');
+let modalClose = document.querySelector('.closeModal');
+let line1 = document.querySelector('.linedNav1');
+let line2 = document.querySelector('.linedNav2');
+let line3 = document.querySelector('.linedNav3');
 
+modalButton.addEventListener('click', function() {
+    if (clicked == false) {
+        clicked = true;
+        console.log("clicked!");
+        modalPop.style.cssText = "display: grid";
+        line1.style.cssText = "background-color: white; animation-name: nav1; animation-duration: .5s; animation-fill-mode: forwards;"
+        line2.style.cssText = "background-color: white; animation-name: nav2; animation-duration: .5s; animation-fill-mode: forwards;"
+        line3.style.cssText = "background-color: white; animation-name: nav3; animation-duration: .5s; animation-fill-mode: forwards;"
 
+    } else if (clicked == true) {
+        modalPop.style.cssText = "display: none";
+        line1.style.cssText = '';
+        line2.style.cssText = '';
+        line3.style.cssText = '';
+        clicked = false;
+
+    }
+   
+    
+})
+
+modalClose.addEventListener('click', function() {
+    console.log("closed!");
+    modalPop.style.cssText = "display: none";
+    line1.style.cssText = '';
+        line2.style.cssText = '';
+        line3.style.cssText = '';
+        clicked = false;
+})
